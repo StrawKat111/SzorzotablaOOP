d54fd570c201da3d3317057d1a91e87a01baa659
ezegy <ezegy@DESKTOP-9POM430>
2024-11-03 19:57:10 +0100

    Add project files.

---
+++ SzorzotablaOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SzorzotablaOOP;
+
+namespace SzorzotablaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] eredmenyek;
+
+            Szorzotabla szorzotabla2 = new Szorzotabla();
+            szorzotabla2.SetSzam(5);
+            szorzotabla2.SetEredmény();
+            eredmenyek = szorzotabla2.GetEredmény();
+            Console.WriteLine("A 3-as szorzótábla:");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{i} * 3 = {eredmenyek[i]}");
+            }
+        }
+    }
+}
---
+++ SzorzotablaOOP/Szorzotabla.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzorzotablaOOP
+{
+    internal class Szorzotabla
+    {
+        private int szam;
+        private int[] eredmeny;
+
+        
+        public Szorzotabla() { }
+
+        
+        public Szorzotabla(int szam)
+        {
+            SetSzam(szam);
+        }
+
+        
+        public void SetSzam(int szam)
+        {
+            if (szam >= 1 && szam <= 9)
+            {
+                this.szam = szam;
+            }
+        }
+
+        
+        public void SetEredmény()
+        {
+            eredmeny = new int[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                eredmeny[i] = szam * i;
+            }
+        }
+
+        
+        public int[] GetEredmény()
+        {
+            return eredmeny;
+        }
+    }
+}
